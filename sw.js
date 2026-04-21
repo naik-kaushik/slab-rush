61e5a7b2e5edced3a998a556446246d812ae2c31
Kaushik Naik <87433036+naik-kaushik@users.noreply.github.com>
2026-04-21 21:24:51 +0530

    Merge pull request #22 from naik-kaushik/enhancement/fix-cache-clear-to-get-new-version
    Improve service worker js to make sure updates are delivered

--- sw.js
+++ sw.js
@@ -1,18 +1,33 @@
-const CACHE_NAME = 'slab-rush-v1';
+const CACHE_NAME = 'slab-rush-1.7.0';
 const ASSETS = [
-    './',
-    './index.html',
-    './css/style.css',
-    './css/carbon.css',
-    './js/game.js',
-    './assets/favicon/site.webmanifest',
-    './assets/favicon/favicon.ico'
+    './?v=1.7.0',
+    './index.html?v=1.7.0',
+    './css/style.css?v=1.7.0',
+    './css/carbon.css?v=1.7.0',
+    './js/game.js?v=1.7.0',
+    './assets/favicon/site.webmanifest?v=1.7.0',
+    './assets/favicon/favicon.ico?v=1.7.0'
 ];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
